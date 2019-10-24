a63e8b59e6ce0b87d237a74b7e858b21829725a1
HokieGeek <andres.f.perez@gmail.com>
2019-10-24 06:36:33 -0400

    Added better error reporting cause these are not showing up on cloudwatch predictably well

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -13,17 +14,21 @@ import (
 	nexusiq "github.com/sonatype-nexus-community/gonexus/iq"
 )
 
-func requestError(statusCode int) *events.APIGatewayProxyResponse {
+func requestError(statusCode int, message string) *events.APIGatewayProxyResponse {
+	body := message
+	if body == "" {
+		body = http.StatusText(statusCode)
+	}
 	return &events.APIGatewayProxyResponse{
 		StatusCode: statusCode,
-		Body:       http.StatusText(statusCode),
+		Body:       body,
 	}
 }
 
 func getGitHubPullRequest(req events.APIGatewayProxyRequest) (event githubPullRequest, resp *events.APIGatewayProxyResponse) {
 	eventType, err := getGitHubEventType(req.Headers)
 	if err != nil {
-		return event, requestError(http.StatusBadRequest)
+		return event, requestError(http.StatusBadRequest, fmt.Sprintf("could not parse request headers: %v", err))
 	}
 
 	// x-github-event: pull_request
@@ -33,18 +38,18 @@ func getGitHubPullRequest(req events.APIGatewayProxyRequest) (event githubPullRe
 		return event, &events.APIGatewayProxyResponse{StatusCode: http.StatusOK}
 	case eventType != "pull_request":
 		log.Println("error: did not receive a supported github event")
-		return event, requestError(http.StatusBadRequest)
+		return event, requestError(http.StatusBadRequest, "did not receive a supported github event")
 	}
 
 	decoded, err := url.QueryUnescape(req.Body)
 	if err != nil {
 		log.Println(err)
-		return event, requestError(http.StatusBadRequest)
+		return event, requestError(http.StatusBadRequest, fmt.Sprintf("error during url unescape of payload: %v", err))
 	}
 	payload := strings.TrimPrefix(decoded, "payload=")
 
 	if err = json.Unmarshal([]byte(payload), &event); err != nil {
-		return event, requestError(http.StatusInternalServerError)
+		return event, requestError(http.StatusBadRequest, fmt.Sprintf("could not unmarshal payload as json: %v", err))
 	}
 
 	return event, nil
@@ -96,13 +101,13 @@ func handleLambdaEvent(req events.APIGatewayProxyRequest) (events.APIGatewayProx
 	files, err := getPullRequestFiles(event, token)
 	if err != nil {
 		log.Printf("ERROR: could not get files from pull request: %v\n", err)
-		return *requestError(http.StatusInternalServerError), nil
+		return *requestError(http.StatusInternalServerError, fmt.Sprintf("could not get files from pull request: %v\n", err)), nil
 	}
 
 	components, err := findComponentsFromManifest(files)
 	if err != nil {
 		log.Printf("ERROR: could not read files to find manifest: %v\n", err)
-		return *requestError(http.StatusInternalServerError), nil
+		return *requestError(http.StatusInternalServerError, fmt.Sprintf("could not read files to find manifest: %v\n", err)), nil
 	}
 
 	iqURL := req.QueryStringParameters["iq_server"]
@@ -110,14 +115,14 @@ func handleLambdaEvent(req events.APIGatewayProxyRequest) (events.APIGatewayProx
 	iq, err := nexusiq.New(iqURL, iqAuth[0], iqAuth[1])
 	if err != nil {
 		log.Printf("ERROR: could not create IQ client: %v\n", err)
-		return *requestError(http.StatusInternalServerError), nil
+		return *requestError(http.StatusInternalServerError, fmt.Sprintf("could not create IQ client: %v\n", err)), nil
 	}
 
 	iqApp := req.QueryStringParameters["iq_app"]
 	remediations, err := evaluateComponents(iq, iqApp, components)
 	if err != nil {
 		log.Printf("ERROR: could not evaluate components: %v\n", err)
-		return *requestError(http.StatusInternalServerError), nil
+		return *requestError(http.StatusInternalServerError, fmt.Sprintf("could not evaluate components: %v\n", err)), nil
 	}
 
 	return *addRemediationsToPR(token, event, remediations), nil
