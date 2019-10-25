44a57ab04dcc878b9cf815dad929d3ba150170f3
HokieGeek <andres.f.perez@gmail.com>
2019-10-25 16:58:33 -0400

    Updated helper scripts

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 name=iq-github-remediating-component
 roleArn=$(aws --profile admin iam get-role --role-name lambda_basic_execution |jq '.Role.Arn'|sed 's/\"//g')
 
-GOARCH=amd64 GOOS=linux go build .
+GOARCH=amd64 GOOS=linux go build . || exit 1
 
 zip ${name}.zip ${name}
 aws --profile admin lambda get-function --function-name ${name} && {
